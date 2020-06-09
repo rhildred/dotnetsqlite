8ecc6efc57218bac6cf89c49cd3dbdb677c16795
Rich Hildred <rhildred@gmail.com>
2020-06-09 13:10:33 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace Jun9sqlitedotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("Data Source=hello.db"))
+            {
+                int nId = 1;
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText =
+                @"
+        SELECT name
+        FROM user
+        WHERE id = $id
+    ";
+                command.Parameters.AddWithValue("$id", nId);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name = reader.GetString(0);
+
+                        Console.WriteLine($"Hello, {name}!");
+                    }
+                }
+            }
+        }
+    }
+}
